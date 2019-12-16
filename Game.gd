3fc5f0368290b442b1e9b2ebdc37f33f529b0b8d
Linko-3D <dbekhouc@gmail.com>
2019-12-16 16:04:57 +0100

    tweak

--- Game.gd
+++ Game.gd
@@ -50,7 +50,7 @@ func _process(delta):
 		$Rings/Ring7.play("metal")
 	
 	if $CameraAnimation.is_playing() == false:
-		camera.rotate_y(-.01)
+		camera.rotate_y(-.5*delta)
 		
 	if wasPlayed == false:
 		if message == 7:
